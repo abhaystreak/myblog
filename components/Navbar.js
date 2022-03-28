53bac9a02de4ceea46767a9466d121de8fe6dbe8
abhaystreak <86460017+abhaystreak@users.noreply.github.com>
2022-03-28 20:37:03 +0530

    Add files via upload

---
+++ components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+    <div className="nav-wrapper #b2ebf2 cyan lighten-4
+">
+      <a href="#" className="brand-logo">Logo</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+    </nav>  
+    
+  )
+}
